d4a7dfe8ca172c5aa82661f2c95e031c0129be10
Diego Occhi <diego.occhi@devionn.com>
2019-05-03 10:29:19 -0300

    aula 2

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides } from '@ionic/angular';
+import { IonSlides, ToastController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,94 @@ import { IonSlides } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   @ViewChild(IonSlides) slides: IonSlides;
+  // @ViewChild('meuSlides') slides: IonSlides;
 
-  constructor() { }
+  public formCadastro: FormGroup;
+  public email: string;
+  public senha: string;
 
-  ngOnInit() { }
+  constructor(public formBuilder: FormBuilder,
+              public toastCtrl: ToastController) { }
+
+  ngOnInit() { 
+
+    this.formCadastro = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, Validators.required],
+      senha: [null, Validators.required]
+    });
+
+  }
 
   public registrar() {
     this.slides.slideNext();
   }
 
   public cancelar() {
+    this.formCadastro.reset();
     this.slides.slidePrev();
   }
+
+  public salvar() {
+
+    let cadastros: Array<any> = new Array();
+    let cadastrosStorage = localStorage.getItem('cadastros');
+
+    if (cadastrosStorage) {
+      cadastros = JSON.parse(cadastrosStorage);
+    }
+
+    cadastros.push(this.formCadastro.value);
+
+    localStorage.setItem('cadastros', JSON.stringify(cadastros));
+
+    this.cancelar();
+
+    this.toastCtrl.create({
+      message: 'Usuário salvo com sucesso!',
+      duration: 3000
+    }).then(toast => {
+      toast.present();
+    });
+  }
+
+  public entrar() {
+
+    let cadastrosStorage = localStorage.getItem('cadastros');
+    let cadastros: Array<any> = new Array();
+
+    if (cadastrosStorage) {
+      cadastros = JSON.parse(cadastrosStorage);
+    }
+    
+    let achou = false;
+    cadastros.forEach((cadastro, index) => {
+      if (cadastro.email === this.email && cadastro.senha === this.senha) {
+        achou = true;
+        localStorage.setItem('logado', JSON.stringify(cadastro));
+      }
+    });
+
+    if (achou) {
+
+      this.toastCtrl.create({
+        message: 'Login realizado com sucesso!',
+        duration: 2000
+      }).then(toast => {
+        toast.present();
+      });
+      
+    } else {
+
+      this.toastCtrl.create({
+        message: 'Login inválido!',
+        duration: 2000
+      }).then(toast => {
+        toast.present();
+      });
+
+    }
+
+  }
+
 }
